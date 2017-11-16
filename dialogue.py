1e6adadc6765d339ebbd7ca650d9b435d56fb366
Anonymous <none>
2017-11-16 14:26:30 -0800

    whatever

--- dialogue.py
+++ dialogue.py
@@ -2,16 +2,16 @@
 from utils import read_multi, write_multi
 
 
-table = ("****************"
-         "********　*あいうえおか"
+table = ("****\n***********"
+         "******ＢＲ　*あいうえおか"
          "きくけこさしすせそたちつてとなに"
          "ぬねのはひふへほまみむめもやゆよ"
          "らりるれろわをんっゃゅょ****"
          "****************"
-         "****************"
-         "******ー**？！「」***"
+         "*********エモン*ス**"
+         "******ー*・？！「」***"
          "********がぎぐげござじず"
-         "ぜぞだぢづでどばびぶべぼ****"
+         "ぜぞだぢづでどばびぶべぼゴビ**"
          "********ぱぴぷぺぽ***"
          )
 table = table.decode('utf-8')
@@ -43,13 +43,14 @@ class Dialogue:
         s = "%x %s" % (self.pointer, s)
         return s
 
-    def read_data(self, filename):
+    def read_data(self, filename, header=False):
         f = open(filename, 'r+b')
         f.seek(self.pointer)
-        unknown = f.read(4)
-        self.y = f.read(2)
-        self.x = f.read(2)
-        self.ff = f.read(1)
+        if header:
+            unknown = f.read(4)
+            self.y = f.read(2)
+            self.x = f.read(2)
+            self.ff = f.read(1)
         nowpointer = f.tell()
         message = []
         while True:
@@ -59,9 +60,9 @@ class Dialogue:
                 # stored word
                 wordlength = (byte & 0x0F) + 4
                 ctrlptr = read_multi(f, length=2)
-                if ctrlptr >= 0x8000:
-                    print "WARNING %x" % ctrlptr
-                f.seek(0x1b0000 | ctrlptr)
+                #if ctrlptr >= 0x8000:
+                #    print "WARNING1 %x" % ctrlptr
+                f.seek((nowpointer & 0xff0000) | ctrlptr)
                 word = map(ord, f.read(wordlength))
                 message.extend(word)
                 nowpointer += 3
@@ -74,24 +75,43 @@ class Dialogue:
             elif byte & 0xF0 == 0xB0:
                 # control command
                 nowpointer += 3
+            elif byte & 0xF0 == 0xC0:
+                message.append(0x18)
+                nowpointer += 1
             elif byte & 0xF0 == 0xD0:
                 message.append(byte)
                 nowpointer += 1
             elif byte == 0x00:
                 # termination
+                nowpointer += 1
                 break
             elif byte < 0xb0:
                 # ordinary character
                 message.append(byte)
                 nowpointer += 1
             else:
-                print "WARNING %x" % byte
+                print "WARNING2 %x" % byte
                 nowpointer += 1
         f.close()
         self.message = message
+        return nowpointer
 
 
 if __name__ == "__main__":
-    d = Dialogue(pointer=0x1b570f)
-    d.read_data("test.smc")
-    print unicode(d)
+    from sys import argv
+    try:
+        #pointer = 0x1ee132
+        pointer = int(argv[1], 0x10)
+        #d = Dialogue(pointer=0x1b5337)
+        #for i in range(547):
+        #for i in range(547)[:450]:
+        for i in range(3):
+            d = Dialogue(pointer=pointer)
+            pointer = d.read_data("test.smc")
+            print unicode(d)
+    except ValueError:
+        searchfor = argv[1].decode('utf-8')
+        for c in searchfor:
+            print c,
+            i = table.index(c)
+            print "%x" % i
